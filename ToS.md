78dd761b3e8d09c1c8f5d29eac0a92a36446c64c
Perry Harrington <pedward@apsoft.com>
2020-03-16 18:14:02 -0700

    Code refinements to comply with YouTube ToS
    * The CSS has been modified to ensure the player is at least 200x200px in size.
    * A ToS.md was added with Terms of Service, to comply with YouTube's API ToS
    * The player was altered to provide exclusive access to one player on a page,
    when a single player is activated, it disables all other players until the
    active player has completed viewing a video.  This is a requirement of YT
    embedded video player API usage.

---
+++ ToS.md
@@ -0,0 +1,14 @@
+# Terms of Service
+
+This file is to satisfy the Youtube developer ToS requirements.
+
+1. This API code is provided without warranty, expressed or implied
+2. You may not use this code to access information you are not authorized to access
+3. You may not use this code to access any service run by apsoft.com
+4. You must run this code on your own site or CDN, it may not be run from apsoft.com
+
+As a Youtube API user, you are bound by YouTube Terms of Service, those can be found here:
+
+https://www.youtube.com/static?template=terms
+
+
--- youtube_player_multi_channel.js
+++ youtube_player_multi_channel.js
@@ -1,71 +1,137 @@
-	/* Derived from code/concept by @labnol */
-    /* Web: https://www.labnol.org/internet/light-youtube-embeds/27941/ */
+/* Based on code/concept by @labnol */
+/* Web: http://labnol.org/?p=27941 */
 
-	function setLatestVideo() {
-            var channel,id,div, n,
-                v = document.getElementsByClassName("youtube-player");
-            for (n = 0; n < v.length; n++) {
-                div = document.createElement("div");
-				channel=v[n].dataset.id;
-				getJSONVidId(channel,v[n],div);
-            }
+var active_player = null;
+
+function setLatestVideo() {
+	var channel,id,div, n,
+		v = document.getElementsByClassName("youtube-player");
+	for (n = 0; n < v.length; n++) {
+		div = document.createElement("div");
+		channel=v[n].dataset.id;
+		getJSONVidId(channel,v[n],div);
 	}
+}
 
-	function trunc(str, length) {
-		return str.length > length ? str.substring(0, length - 3) + '...' : str
-	}
+function trunc(str, length) {
+	return str.length > length ? str.substring(0, length - 3) + '...' : str
+}
 
-	function getJSONVidId(channelId,element,div) {
-      var obj = {
+function getJSONVidId(channelId,element,div) {
+	var obj = {
 		'part': 'id,snippet',
-		'key':'YOUTUBE API KEY',
-      'channelId': channelId,
-      'maxResults': 1,
-      'order': 'date'
-		};
-	  	var url="https://www.googleapis.com/youtube/v3/search?";
-	  	//var url="https://example.com/path/to/ytapi_cache.php?endpoint=search&";
-		jQuery.getJSON(url+jQuery.param(obj),function(result) {
-				var id=result.items[0].id.videoId,
-				title=trunc(result.items[0].snippet.title,30);
-				div.setAttribute("data-id", id);
-                div.innerHTML = labnolThumb(id);
-				div.getElementsByClassName("title")[0].innerHTML=title;
-                div.onclick = labnolIframe;
-                element.appendChild(div);
-		}).then(function() {
-      var obj = {
-		'part': 'snippet',
-		'key':'YOUTUBE API KEY',
-      'id': channelId
-		};
-	  	var url="https://www.googleapis.com/youtube/v3/channels?";
-	  	//var url="https://example.com/path/to/ytapi_cache.php?endpoint=channels&";
-		jQuery.getJSON(url+jQuery.param(obj),function(result) {
-				var thumb=result.items[0].snippet.thumbnails.default.url,name=result.items[0].snippet.title;
-				div.getElementsByClassName("thumb")[0].style.backgroundImage='url('+thumb+')';
-				div.getElementsByClassName("name")[0].innerHTML=name;
-		});
-		});
-			
+		'key':'API KEY',
+		'channelId': channelId,
+		'maxResults': 1,
+		'order': 'date'
+	};
+	//var url="https://www.googleapis.com/youtube/v3/search?";
+	var url="https://example.com/path/to/ytapi_cache.php?endpoint=search&";
+	jQuery.getJSON(url+jQuery.param(obj),function(result) {
+			var id=result.items[0].id.videoId,
+			title=trunc(result.items[0].snippet.title,30);
+			div.setAttribute("data-id", id);
+			div.setAttribute("class", "inactiveplayer");
+			div.setAttribute("id", id);
+			div.innerHTML = labnolThumb(id);
+			div.getElementsByClassName("title")[0].innerHTML=title;
+			div.onclick = labnolIframe;
+			element.appendChild(div);
+			}).then(function() {
+				var obj = {
+				'part': 'snippet',
+				'key':'API KEY',
+				'id': channelId
+				};
+				//var url="https://www.googleapis.com/youtube/v3/channels?";
+				var url="https://example.com/path/to/ytapi_cache.php?endpoint=channels&";
+				jQuery.getJSON(url+jQuery.param(obj),function(result) {
+						var thumb=result.items[0].snippet.thumbnails.default.url,name=result.items[0].snippet.title;
+						div.getElementsByClassName("thumb")[0].style.backgroundImage='url('+thumb+')';
+								div.getElementsByClassName("name")[0].innerHTML=name;
+								});
+						});
+
+				}
+
+				document.addEventListener("DOMContentLoaded", setLatestVideo);
+
+				function labnolThumb(id) {
+				var divs = '<img src="//i.ytimg.com/vi/ID/hqdefault.jpg">'+
+					'<div class="title"></div>'+
+					'<div class="play"></div>'+
+					'<div class="thumb"></div>'+
+					'<div class="name"></div>';
+				return divs.replace("ID", id);
+				}
+
+var api_loaded = null;
+function labnolIframe() {
+
+	this.setAttribute("class", "activeplayer");
+
+	if (api_loaded === null) {
+		var tag = document.createElement('script');
+
+		tag.src = "https://www.youtube.com/iframe_api";
+		var firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+		api_loaded = true;
+	} else {
+		if (active_player === null) {
+			return onYouTubeIframeAPIReady();
+		}
+	}
+}
+
+var player = [];
+function onYouTubeIframeAPIReady() {
+	// disable all inactive players so only 1 video can play at a time
+	var n,v = document.getElementsByClassName("inactiveplayer");
+	for (n = 0; n < v.length; n++) {
+		var div = v[n];
+		div.style.opacity="0.5";
+		div.style.pointerEvents="none";
 	}
 
-    document.addEventListener("DOMContentLoaded", setLatestVideo);
+	// create the video player for the active element
+	var n,v = document.getElementsByClassName("activeplayer");
+	for (n = 0; n < v.length; n++) {
+		var div = v[n];
+		player[div.id] = new YT.Player(div.id, {
+			videoId: div.id,
+			events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+			}
+		});
+		player[div.id].xvideoId=div.id;
+	}
 
-    function labnolThumb(id) {
-        var divs = '<img src="//i.ytimg.com/vi/ID/hqdefault.jpg">'+
-            		'<div class="title"></div>'+
-            		'<div class="play"></div>'+
-					'<div class="thumb"></div>'+
-            		'<div class="name"></div>';
-        return divs.replace("ID", id);
-    }
+}
 
-    function labnolIframe() {
-        var iframe = document.createElement("iframe");
-        var embed = "//www.youtube.com/embed/ID?autoplay=1";
-        iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
-        iframe.setAttribute("frameborder", "0");
-        iframe.setAttribute("allowfullscreen", "1");
-        this.parentNode.replaceChild(iframe, this);
-    }
+function onPlayerReady(event) {
+	event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+	if (event.data == YT.PlayerState.PLAYING && active_player === null) {
+		// set exclusive owner of play ability
+		active_player = event.target;
+
+	} else if (event.data == YT.PlayerState.ENDED && active_player !== null) {
+		active_player = null;
+
+		// reset active player to inactive when playing is done
+		document.getElementById(event.target.xvideoId).setAttribute("class", "inactiveplayer");
+
+		// make all other video player able to accept input
+		var n,v = document.getElementsByClassName("inactiveplayer");
+		for (n = 0; n < v.length; n++) {
+			var div = v[n];
+			div.style.opacity="1";
+			div.style.pointerEvents="auto";
+		}
+	}
+}
+
